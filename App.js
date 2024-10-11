2b645d87c152c3fdb66cad5ec9c7fc76905f71f8
abmah <ab.mahasnh@gmail.com>
2024-10-11 21:46:57 +0300

    'font'

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
+import React, { useCallback } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import { StatusBar } from "react-native";
+import { StatusBar, View, Text, TextInput } from "react-native";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (<>
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor="#0F1F26"
-      translucent={true}
-    />
-    <NavigationContainer>
-      <BottomTabNavigator />
-    </NavigationContainer>
-  </>
+  // Load custom fonts
+  const [fontsLoaded] = useFonts({
+    'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+    'Nunito-SemiBold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+    'Nunito-ExtraBold': require('./assets/fonts/Nunito-ExtraBold.ttf'),
+    'Nunito-Black': require('./assets/fonts/Nunito-Black.ttf'),
+  });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#0F1F26"
+        translucent={true}
+      />
+      <NavigationContainer>
+        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+          <BottomTabNavigator />
+        </View>
+      </NavigationContainer>
+    </>
   );
 }
-
